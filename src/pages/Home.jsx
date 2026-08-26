730716a31bcceb5c00f326f2fc656e63dd537f71
Ahmed Elmallah <ahmedwaheedelmallah@gmail.com>
2026-08-26 14:20:45 +0300

    feat(categories): finish categories section ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/home/Hero";
+import CategorySection from "../components/home/CategorySection";
 
 const Home = () => {
   return (
     <div className="min-h-screen">
       <Hero />
+      <CategorySection />
     </div>
   );
 };
